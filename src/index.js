e2ec360be893b14d52fe5912ce3eaa7375bb291e
fadilnatakusumah <fadil.ntksmh@gmail.com>
2020-11-09 12:48:50 +0700

    set to heroku

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const morgan = require('morgan')
+const morgan = require('morgan');
+const path = require('path');
 
 const connectDB = require('../config/db');
 
@@ -17,6 +18,12 @@ const apiRoutes = require('../routes/api');
 
 app.use('/api', apiRoutes);
 
-app.get('/', (req, res) => res.send("API RUNNING"))
+
+// serve static asssets to production
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+}
 
 app.listen(PORT, () => console.info(`Server running on port ${PORT}`))
